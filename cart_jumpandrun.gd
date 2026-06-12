30f15898742ae47f5adfba943c248cb70941fcca
Glace2222 <clarabodenhausen@Mac.fritz.box>
2026-06-12 18:28:12 +0200

    test zur drehung

--- cart_jumpandrun.gd
+++ cart_jumpandrun.gd
@@ -12,5 +12,8 @@ func _process(delta: float) -> void:
 		get_tree().reload_current_scene()
 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func _on_area_respawn_body_entered(body: Node2D) -> void:
 	get_tree().reload_current_scene()
+
+
+	
