19d38747514de728e322b786c6e31b10e0b495c7
Glace2222 <clarabodenhausen@Mac.fritz.box>
2026-06-12 16:31:45 +0200

    dos not fall

--- cart.gd
+++ cart.gd
@@ -1,25 +1,24 @@
+class_name Cart
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+const gravity = 8.1
+const jump_force = -240
 
 func _physics_process(delta: float) -> void:
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if Input.is_key_pressed(KEY_A):
+		velocity.x = -50
+		$AnimatedSprite2D.flip_h = true
+	elif Input.is_key_pressed(KEY_D):
+		velocity.x = 50
+		$AnimatedSprite2D.flip_h = false
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = 0
+		
+	if Input.is_key_pressed(KEY_SPACE) and is_on_floor():
+		velocity.y = jump_force
+		
+		
+	velocity.y += gravity
 	move_and_slide()
+	
+		
