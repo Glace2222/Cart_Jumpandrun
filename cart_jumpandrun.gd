44ef1ca3c4fe075463bad7bf59cbe62333d56e28
Glace2222 <clarabodenhausen@Mac.fritz.box>
2026-06-14 19:14:55 +0200

    Camera & Fullscreen

--- cart_jumpandrun.gd
+++ cart_jumpandrun.gd
@@ -3,15 +3,12 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_key_label_pressed(KEY_R):
 		get_tree().reload_current_scene()
-
-
 
 
 
@@ -22,5 +19,8 @@ func _on_area_respawn_body_entered(body: Node2D) -> void:
 	get_tree().reload_current_scene()
 
 
-func _on_area_2d_zielflagge_body_entered(body: Node2D) -> void:
-	$YouWon.show()
+
+
+
+func _on_area_steigung_body_entered(body: Node2D) -> void:
+	pass
